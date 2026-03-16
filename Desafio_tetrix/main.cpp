a45448caa9211aa64960c6e20f6a1948dddd2bcb
cbaena-alt <c.baena@udea.edu.co>
2026-03-16 17:43:57 -0500

    funciones para el movimiento de las piezas

--- Desafio_tetrix/main.cpp
+++ Desafio_tetrix/main.cpp
@@ -3,14 +3,5 @@
 using namespace std;
 
 int main()
-{
-    cout << "Hello World!" << endl;
-
-    cout<<"hola cristian baena"<<endl;
-
-    cout<<"prueba 00111"<<endl;
-    cout<<"laura hola 0990"<<endl;
-    return 0;
-}
 
 
--- Desafio_tetrix/piezas.h
+++ Desafio_tetrix/piezas.h
@@ -1,10 +1,17 @@
 #ifndef PIEZAS_H
 #define PIEZAS_H
 
-class piezas
-{
+class Pieza {
 public:
-    piezas();
+    unsigned short forma; // Usamos 16 bits para una rejilla de 4x4
+    int x, y;            // Posición en el tablero
+
+    Pieza();
+    void cargarCuadrado();   // Pieza O
+    void cargarLinea();      // Pieza I
+    void moverDerecha();     // Usará >>
+    void moverIzquierda();   // Usará <<
+    void rotar();            // El reto de bits
 };
 
-#endif // PIEZAS_H
+#endif
