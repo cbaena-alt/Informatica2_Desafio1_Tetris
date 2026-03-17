b1a87ee2a5644a5c419106ac000a0f6b1d406c8c
laurafuentes2-lgtm <laura.fuentes2@udea.edu.co>
2026-03-16 23:43:35 -0500

    memoria dinamica y imprimir tablero
    Implementación de la clase Tablero con memoria dinámica y función de visualización

--- Desafio_tetrix/tablero.cpp
+++ Desafio_tetrix/tablero.cpp
@@ -1,3 +1,32 @@
 #include "tablero.h"
+//pedimos la memoria
 
-tablero::tablero() {}
+ Tablero::Tablero(int f, int c) {
+    filas = f;
+    columnas = c;
+    malla = new unsigned char*[filas];
+    for (int i = 0; i < filas; i++) {
+        malla[i] = new unsigned char[columnas];
+        for (int j = 0; j < columnas; j++) {
+            malla[i][j] = 0;
+        }
+     }
+ }
+
+ //Devolvemos la memoria
+ Tablero ::~Tablero (){
+     for (int i = 0; i< filas; i++){
+         delete[] malla [i];
+     }
+     delete [] malla;
+ }
+
+ void Tablero:: mostrar() {
+     for (int i = 0; i< filas; i++){
+         for(int j=0; j< columnas; j++ ){
+             std::cout<<(int)malla[i][j]<<" ";
+         }
+         std::cout <<std::endl;
+     }
+ }
+
--- Desafio_tetrix/tablero.h
+++ Desafio_tetrix/tablero.h
@@ -1,10 +1,18 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-class tablero
-{
-public:
-    tablero();
-};
+#include <iostream>
+class Tablero {
+private:
+    int filas;
+    int columnas;
+    unsigned char** malla;
 
+public:
+    Tablero(int f = 20, int c = 10);
+    ~Tablero();
+
+    void mostrar();
+
+};
 #endif // TABLERO_H
